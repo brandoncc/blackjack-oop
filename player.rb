88bc445dda3729cde9997dfc37eff265fc17689c
Brandon Conway <brandoncc@gmail.com>
2013-12-06 20:40:26 -0800

    initialize player

--- player.rb
+++ player.rb
@@ -1,3 +1,16 @@
 class Player
-  
+  attr_accessor :name
+
+  def initialize
+    puts 'Hello, what is your name?'
+
+    input = gets.chomp
+    while input.length.zero?
+      puts "I'm sorry, I didn't get that."
+      input = gets.chomp
+    end
+    @name = input
+
+    puts "Nice to meet you, #{name}"
+  end
 end
