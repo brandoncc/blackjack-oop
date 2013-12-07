bdc6d0b4d448048ff743ed8a772a214b60241c78
Brandon Conway <brandoncc@gmail.com>
2013-12-06 20:15:59 -0800

    initialize game

--- blackjack.rb
+++ blackjack.rb
@@ -1,3 +1,13 @@
+require_relative 'card'
+require_relative 'dealer'
+require_relative 'deck'
+require_relative 'hand'
+require_relative 'player'
+
 class Blackjack
-  
+  def initialize
+    @player = Player.new
+    @dealer = Dealer.new
+    @deck   = Deck.new
+  end
 end
