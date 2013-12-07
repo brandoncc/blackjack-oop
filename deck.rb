1fef96d409b157f25d3feb11e5709abde28dfc10
Brandon Conway <brandoncc@gmail.com>
2013-12-07 00:27:56 -0800

    fix duplicate card object bug

--- deck.rb
+++ deck.rb
@@ -6,12 +6,13 @@ class Deck
     @shuffled_cards = []
     @discard_pile = []
 
+    how_many_decks.times do
       %w(Clubs Diamonds Hearts Spades).each do |s|
         %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace).each do |v|
+        end
       end
     end
 
-    @shuffled_cards *= how_many_decks
     15.times { @shuffled_cards.shuffle! }
   end
 end
