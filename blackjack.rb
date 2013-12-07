5aa32989321c1b85b482e2780f031e86aa088786
Brandon Conway <brandoncc@gmail.com>
2013-12-06 20:39:21 -0800

    choose game type

--- blackjack.rb
+++ blackjack.rb
@@ -9,5 +9,30 @@ class Blackjack
     @player = Player.new
     @dealer = Dealer.new
     @deck   = Deck.new
+
+    loop do
+      puts ''
+      puts 'Would you like to play (h) home rules or (v) rules?'
+      game_type = gets.chomp
+
+      case game_type.downcase
+        when 'h'
+          play_home_rules
+          break
+        when 'v'
+          puts 'Sorry, this feature is not included in this version.'
+          # TODO: break (after feature is implemented)
+        else
+          puts "Sorry, '#{game_type}' is not a valid option."
+      end
+    end
+  end
+
+  def play_home_rules
+    puts 'You chose home rules.'
+  end
+
+  def play_vegas_rules
+    puts 'You chose Vegas rules.'
   end
 end
