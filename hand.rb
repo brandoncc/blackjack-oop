ca073aa120a41d3712125e5b42cdeb5f5d85f087
Brandon Conway <brandoncc@gmail.com>
2013-12-07 00:31:23 -0800

    add calculate_value method

--- hand.rb
+++ hand.rb
@@ -1,3 +1,18 @@
 # hand class
 class Hand
+
+  def calculate_value
+    score      = { value: 0, soft_or_hard: nil }
+    aces_count = 0
+
+    @cards.each do |card|
+      if card.value == 11
+        aces_count += 1
+      else
+        score[:value] += card.value
+      end
+    end
+
+    calculate_aces(score, aces_count)
+  end
 end
